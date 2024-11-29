7dba5da00a0c164d6cfbe59f05ad2a7c487aa6e7
ankitkarki15 <ankitkarki8088@gmail.com>
2024-11-29 22:59:17 +0545

    updated Project Files and Code

--- companyapi/api/serializers.py
+++ companyapi/api/serializers.py
@@ -16,8 +16,3 @@ class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Employee
         fields="__all__"
-class employeeSerializer(serializers.HyperlinkedModelSerializer):
-    id=serializers.ReadOnlyField()
-    class Meta:
-        model=Employee
-        fields="__all__"
